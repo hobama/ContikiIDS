f0cfb4c1e111ab12de06aaeff6d2a72ebab4a31e
dharmini <rutzara@gmail.com>
2014-05-06 06:16:04 +0200

    added network client

--- examples/ipv6/ids/ids-client/network-mapper-client.c
+++ examples/ipv6/ids/ids-client/network-mapper-client.c
@@ -1,77 +1,180 @@
-/*
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Institute nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- *
- * This file is part of the Contiki operating system.
- *
- */
+#include "ids-client.h"
+#include "ids-common.h"
 
 #include "contiki.h"
+#include "contiki-lib.h"
+#include "contiki-net.h"
 #include "net/uip.h"
-#include "net/uip-ds6.h"
-#include "net/uip-udp-packet.h"
-#include "net/neighbor-info.h"
+#include "net/rpl/rpl.h"
 
-#include "mapper-client.h"
-
-#include <stdio.h>
 #include <string.h>
 
-#define DEBUG DEBUG_PRINT
+#define DEBUG DEBUG_NONE
 #include "net/uip-debug.h"
 
-/*---------------------------------------------------------------------------*/
-PROCESS(mapper_client_process, "UDP client process");
-AUTOSTART_PROCESSES(&mapper_client_process, &mapper_client);
+static struct uip_udp_conn *mapper_conn;
+extern uip_ds6_route_t uip_ds6_routing_table[UIP_DS6_ROUTE_NB];
+extern rpl_instance_t instance_table[];
 
-/*---------------------------------------------------------------------------*/
+PROCESS(mapper_client, "IDS network mapper client");
+AUTOSTART_PROCESSES(&mapper_client);
+
 static void
-set_global_address(void)
+tcpip_handler(void)
 {
-  uip_ipaddr_t ipaddr;
 
-  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
-  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+ 
+  static int i, j;
+  uint16_t tmp_id;
+  PRINTF("tcpip_handler()\n");
+  if(uip_newdata()) {
+    // TODO Check that this is the right port (and perhaps proto?)
+    uint8_t instance_id;
+    uint8_t timestamp;
+    uint16_t dag_id;
+    uint8_t version;
+    uint16_t etx;
 
-  /* set server address */
+    PRINT6ADDR(&UIP_IP_BUF->srcipaddr);
+    PRINTF("the address inserted was \n");
+   /* //dharmini PRINTF("DEBUG %s:%d\n",__FUNCTION__,__LINE__);*/
+    unsigned char * in_data = uip_appdata;
+   
+    MAPPER_GET_PACKETDATA(instance_id, in_data);
+    MAPPER_GET_PACKETDATA(dag_id, in_data);
+    MAPPER_GET_PACKETDATA(version, in_data);
+    MAPPER_GET_PACKETDATA(timestamp, in_data);
+   // MAPPER_GET_PACKETDATA(etx, in_data);
+    //dharmini
+   printf("dharmini-");
+   // PRINTF("DEBUG %s:%d\n",__FUNCTION__,__LINE__);
+    
+    // Go through all RPL instances
+    for (i = 0; i < RPL_MAX_INSTANCES; ++i) {
+      if (instance_table[i].used && instance_table[i].instance_id == instance_id) {
+        // And all their DAGs
+        for (j = 0; j < RPL_MAX_DAG_PER_INSTANCE; ++j) {
+          if (instance_table[i].dag_table[j].used &&
+              compress_ipaddr_t(&instance_table[i].dag_table[j].dag_id) ==
+              dag_id) {
+            if (instance_table[i].dag_table[j].version != version) {
+              PRINTF("Wrong RPL DODAG Version Number\n");
+              return;
+            }
+            // All IPs are compressed to fit in a uint16_t (compress_ipaddr_t)
+            // rpl_rank_t is a uint16_t
+            //
+            // My IP (uint16_t) | IID (uint8_t) | DAG ID (ipaddr_t) |
+            // Dag Ver.  (uint8_t) | Timestamp (uint8_t) | Rank (uint16_t) |etx(uint16_t)
+            // Parent IP (uint16_t) | #neighbors (uint16_t) | NEIGHBORS
+            //
+            // NEIGHBORS = Neighbor ID (uint16_t) | Neighbor rank (uint16_t) | Neighbor etx
+
+            // calculate size of out_data
+            int outdata_size =
+              sizeof(uint16_t) + sizeof(instance_id) + sizeof(dag_id) + sizeof(version) +
+              sizeof(version) + sizeof(timestamp) + sizeof(rpl_rank_t) +
+              sizeof(uint16_t) + sizeof(uint16_t);
+
+            rpl_parent_t *p;
+            for(p = list_head(instance_table[i].dag_table[j].parents);
+                p != NULL; p = list_item_next(p)) {
+              if (p->rank == -1)
+                continue;
+              outdata_size += sizeof(uint16_t) + sizeof(rpl_rank_t) + sizeof(etx);
+            }
+
+            unsigned char out_data[outdata_size];
+            unsigned char * out_data_p = out_data;
+            uip_ipaddr_t * myip;
+            myip = &uip_ds6_get_link_local(ADDR_PREFERRED)->ipaddr;
+            if (myip == NULL) // We have no interface to use
+              return;
+
+            // My IP adress
+            tmp_id = compress_ipaddr_t(myip);
+            MAPPER_ADD_PACKETDATA(out_data_p, tmp_id);
+     
+            // RPL Instance ID | DODAG ID | DODAG Version Number | Timestamp | etx
+            MAPPER_ADD_PACKETDATA(out_data_p, instance_id);
+            MAPPER_ADD_PACKETDATA(out_data_p, dag_id);
+            MAPPER_ADD_PACKETDATA(out_data_p, version);
+            MAPPER_ADD_PACKETDATA(out_data_p, timestamp);
+//dharmini// 
+ 	   //  etx=instance_table[i].dag_table[j].instance->mc.obj.etx;
+            
+            // MAPPER_ADD_PACKETDATA(out_data_p,etx);
+
+            // My rank
+            MAPPER_ADD_PACKETDATA(out_data_p, instance_table[i].dag_table[j].rank);
+
+            // preferred parent
+            PRINTF("parent: ");
+            PRINT6ADDR(&instance_table[i].dag_table[j].preferred_parent->addr);
+            PRINTF("\n");
+
+            tmp_id = compress_ipaddr_t(&instance_table[i].dag_table[j].preferred_parent->addr);
+            MAPPER_ADD_PACKETDATA(out_data_p, tmp_id);
+
+            // Get all potential parents (neighbors) and their ranks
+            uint16_t * neighbors = (uint16_t *)out_data_p;
+            *neighbors = 0;
+            out_data_p += sizeof(neighbors);
+
+            for(p = list_head(instance_table[i].dag_table[j].parents); p !=
+                NULL; p = list_item_next(p)) {
+              if (p->rank == -1)
+                continue;
+              ++(*neighbors);
+              tmp_id = compress_ipaddr_t(&p->addr);
+              MAPPER_ADD_PACKETDATA(out_data_p, tmp_id);
+
+              MAPPER_ADD_PACKETDATA(out_data_p, p->rank);
+
+              PRINT6ADDR(&p->addr);
+              PRINTF(" got rank %d\n", p->rank);
+           
+             etx=(uint16_t)instance_table[i].dag_table[j].instance->mc.obj.etx;
+
+             MAPPER_ADD_PACKETDATA(out_data_p,etx);
+
+            }
+            PRINTF("%d neighbors\n", *neighbors);
+        //    printf("dharmini");
+            uip_udp_packet_sendto(mapper_conn, out_data, sizeof(out_data), &UIP_IP_BUF->srcipaddr, UIP_HTONS(MAPPER_SERVER_PORT));
+            break;
+          }
+        }
+        break;
+      }
+    }
+    /* Ignore incoming data */
+  }
+    
 }
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(mapper_client_process, ev, data)
+
+PROCESS_THREAD(mapper_client, ev, data)
 {
   PROCESS_BEGIN();
 
   PROCESS_PAUSE();
 
-  set_global_address();
+  PRINTF("Mapper-client started\n");
 
-  PRINTF("UDP client process started\n");
+  mapper_conn = udp_new(NULL, UIP_HTONS(MAPPER_SERVER_PORT), NULL);
+  udp_bind(mapper_conn, UIP_HTONS(MAPPER_CLIENT_PORT));
 
+  PRINTF("Created a connection with the server ");
+  PRINT6ADDR(&mapper_conn->ripaddr);
+  PRINTF(" local/remote port %u/%u\n", UIP_HTONS(mapper_conn->lport),
+   UIP_HTONS(mapper_conn->rport));
+  
   while(1) {
     PROCESS_YIELD();
+    if(ev == tcpip_event) {
+      tcpip_handler();
+    }
   }
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
